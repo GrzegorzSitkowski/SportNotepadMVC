fdbb50fa83c494068031c094f7bc1ae7832c1561
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2021-12-30 20:28:18 +0100

    Created project Web

---
+++ SportNotepadMVC.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SportNotepadMVC.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
