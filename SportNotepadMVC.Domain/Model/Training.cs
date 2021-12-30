5c15a86d2285c5e3cbf1f31077292bf7a63bbd8f
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2021-12-30 21:34:16 +0100

    Added model - Training

---
+++ SportNotepadMVC.Domain/Model/Training.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportNotepadMVC.Domain.Model
+{
+    public class Training
+    {
+        public int Id { get; set; }
+        public float Distance { get; set; }
+        public string Result { get; set; }
+        public string AveragePace { get; set; }
+        public string Localization { get; set; }
+        public string Comment { get; set; }
+    }
+}
