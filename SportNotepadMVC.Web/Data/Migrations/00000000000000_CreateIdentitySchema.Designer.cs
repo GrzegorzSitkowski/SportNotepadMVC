f2d28e8307b5b3e3dc2986f612873c45b4abd49a
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-01-02 19:24:01 +0100

    Add migration InitialCreate

--- SportNotepadMVC.Web/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ SportNotepadMVC.Web/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -4,12 +4,13 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using SportNotepadMVC.Infrastructure;
 using SportNotepadMVC.Web.Data;
 using System;
 
 namespace SportNotepadMVC.Web.Data.Migrations
 {
-    [DbContext(typeof(ApplicationDbContext))]
+    [DbContext(typeof(Context))]
     [Migration("00000000000000_CreateIdentitySchema")]
     partial class CreateIdentitySchema
     {
