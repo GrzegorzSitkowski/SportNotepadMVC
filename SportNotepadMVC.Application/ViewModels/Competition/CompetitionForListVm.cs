81b3f8bb7956d22c836b295e7d01f89be0abcdbf
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-01-03 22:36:36 +0100

    Created CompetitionForListVm

---
+++ SportNotepadMVC.Application/ViewModels/Competition/CompetitionForListVm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportNotepadMVC.Application.ViewModels.Competition
+{
+    public class CompetitionForListVm
+    {
+        public int Id { get; set; }
+        public int Position { get; set; }
+        public string Result { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public float Distance { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
