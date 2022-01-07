e1d1f1f50bd516d19c36407c99817a917d05842e
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-01-07 22:43:55 +0100

    Created ITrainingRepository

---
+++ SportNotepadMVC.Domain/Interfaces/ITrainingRepository.cs
@@ -0,0 +1,18 @@
+﻿using SportNotepadMVC.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportNotepadMVC.Domain.Interfaces
+{
+    public interface ITrainingRepository
+    {
+        int AddTraining(Training traing);
+        void EditTraining(Training training);
+        void DeleteTraining(int id);
+        Training GetTrainingById(int trainingId);
+        IQueryable<Training> GetAllTraining();
+    }
+}
