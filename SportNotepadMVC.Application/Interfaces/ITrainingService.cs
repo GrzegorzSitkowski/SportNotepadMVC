03f94b6386857b2eeea3a1d7600e80d5fc977a6d
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-01-08 19:35:14 +0100

    Added ITrainingService

---
+++ SportNotepadMVC.Application/Interfaces/ITrainingService.cs
@@ -0,0 +1,21 @@
+﻿using SportNotepadMVC.Application.ViewModels.Training;
+using SportNotepadMVC.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportNotepadMVC.Application.Interfaces
+{
+    public interface ITrainingService
+    {
+        int AddTraining(NewTrainingVm training);
+        void EditTraining(NewTrainingVm model);
+        void DeleteTraining(int id);
+        TrainingDetailsVm GetTrainingDetails(int id);
+        NewTrainingVm TrainingForEdit(int id);      
+        Training GetTrainingById(int trainingId);
+        ListTrainingForListVm GetAllTrainigs(int pageNo, int pageSize, string searchString);
+    }
+}
