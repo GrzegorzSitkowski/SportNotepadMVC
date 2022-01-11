e29d5e9a7dfdea3c2f11caf2135eb7f665a01f0f
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-01-11 19:30:25 +0100

    Added IProfileUserService

---
+++ SportNotepadMVC.Application/Interfaces/IProfileUserService.cs
@@ -0,0 +1,16 @@
+﻿using SportNotepadMVC.Application.ViewModels.ProfileUser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportNotepadMVC.Application.Interfaces
+{
+    public interface IProfileUserService
+    {
+        int FillProfileUser(NewProfileUserVm profileUser);
+        void EditProfileUser(NewProfileUserVm profileUse);
+        ProfileUserDisplayVm DisplayProfileUser();
+    }
+}
