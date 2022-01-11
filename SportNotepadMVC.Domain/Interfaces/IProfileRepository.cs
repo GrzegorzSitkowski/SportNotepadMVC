9c221c16abdc34a4d73eca04d9d0c745505c1633
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-01-11 18:38:11 +0100

    Added IProfileRepository

---
+++ SportNotepadMVC.Domain/Interfaces/IProfileRepository.cs
@@ -0,0 +1,16 @@
+﻿using SportNotepadMVC.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportNotepadMVC.Domain.Interfaces
+{
+    public interface IProfileRepository
+    {
+        int FillProfile(Profile profile);
+        void EditProfile(Profile profile);
+
+    }
+}
