a7f8180b6325c7558f6d51fd5765ec493b0ed1ed
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-01-24 19:37:40 +0100

    Added ListPhotoGalleryForListVm

---
+++ SportNotepadMVC.Application/ViewModels/PhotoGallery/ListPhotoGalleryForListVm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportNotepadMVC.Application.ViewModels.PhotoGallery
+{
+    public class ListPhotoGalleryForListVm
+    {
+        public PhotoGalleryVm photoGallery { get; set; }
+        public List<PhotoGalleryVm> photoList { get; set; }
+    }
+}
