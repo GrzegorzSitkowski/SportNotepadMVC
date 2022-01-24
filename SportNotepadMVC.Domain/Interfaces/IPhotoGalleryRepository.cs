52f2dfb67af07a87e93bd5cb96eec3c778dec773
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-01-24 19:13:06 +0100

    Added IPhotoGalleryRepository

---
+++ SportNotepadMVC.Domain/Interfaces/IPhotoGalleryRepository.cs
@@ -0,0 +1,18 @@
+﻿using SportNotepadMVC.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportNotepadMVC.Domain.Interfaces
+{
+    public interface IPhotoGalleryRepository
+    {
+        int AddPhoto(PhotoGallery photoGallery);
+        void DeletePhoto(int id);
+        void EditPhoto(int id);
+        PhotoGallery GetPhotoById(int idPhoto);
+        IQueryable<PhotoGallery> GetAllPhotos();
+    }
+}
