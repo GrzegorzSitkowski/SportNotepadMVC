2a3565c3e4fbd478f226c4d34e3d34922bb1b7db
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-01-24 19:36:02 +0100

    Created PhotoGalleryVm

---
+++ SportNotepadMVC.Application/ViewModels/PhotoGallery/PhotoGalleryVm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportNotepadMVC.Application.ViewModels.PhotoGallery
+{
+    public class PhotoGalleryVm
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+}
