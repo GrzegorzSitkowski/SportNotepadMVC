f367e79e5ae3a4b53e6603be15fe33a18a4ef39b
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-01-24 19:15:42 +0100

    Created migration for PhotoGallery

---
+++ SportNotepadMVC.Infrastructure/Repositories/PhotoGalleryRepository.cs
@@ -0,0 +1,40 @@
+﻿using SportNotepadMVC.Domain.Interfaces;
+using SportNotepadMVC.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportNotepadMVC.Infrastructure.Repositories
+{
+    public class PhotoGalleryRepository : IPhotoGalleryRepository
+    {
+        private readonly Context _context;
+
+        public int AddPhoto(PhotoGallery photoGallery)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePhoto(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditPhoto(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<PhotoGallery> GetAllPhotos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PhotoGallery GetPhotoById(int idPhoto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
