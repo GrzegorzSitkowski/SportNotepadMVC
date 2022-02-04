127f84102a3fb8e08ee3de2c6cbd375c81198522
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-02-04 21:12:35 +0100

    Added and filled IGoalRepository

---
+++ SportNotepadMVC.Domain/Interfaces/IGoalRepository.cs
@@ -0,0 +1,18 @@
+﻿using SportNotepadMVC.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportNotepadMVC.Domain.Interfaces
+{
+    public interface IGoalRepository
+    {
+        int AddGoal(Goal goal);
+        int EditGoal(Goal goal);
+        void DeleteGoal(int id);
+        Goal GetGoalById(int idGoal);
+        IQueryable<Goal> GetAllGoals();
+    }
+}
