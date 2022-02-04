3d515816c30a133907a95f24a4737bdaa9e2eeb4
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-02-04 21:03:45 +0100

    Added new model - Goal

---
+++ SportNotepadMVC.Domain/Model/Goal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportNotepadMVC.Domain.Model
+{
+    public class Goal
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+        public string Distance { get; set; }
+        public string Time { get; set; }
+        public string Location { get; set; }
+        public bool Paid { get; set; }
+        public string Comment { get; set; }
+
+    }
+}
