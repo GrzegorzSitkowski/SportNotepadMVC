f0a639da83d11594d237fac80a0e5de2a61f4635
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-02-05 19:48:52 +0100

    Created GoalController

---
+++ SportNotepadMVC.Web/Controllers/GoalController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SportNotepadMVC.Application.Interfaces;
+using SportNotepadMVC.Application.ViewModels.Goal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportNotepadMVC.Web.Controllers
+{
+    public class GoalController : Controller
+    {
+        private readonly IGoalService _goalService;
+
+        public GoalController(IGoalService goalService)
+        {
+            _goalService = goalService;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new NewGoalVm());
+        }
+
+        [HttpPost]
+        public ActionResult Create(NewGoalVm model)
+        {
+            if(ModelState.IsValid)
+            {
+                var goal = _goalService.AddGoal(model);
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+
+        }
+    }
+}
