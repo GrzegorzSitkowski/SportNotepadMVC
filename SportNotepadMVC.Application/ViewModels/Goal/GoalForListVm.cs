6e66c26c9e058c59c5e4905a4db4c347cea7a998
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-02-05 19:11:06 +0100

    Added GoalForListVm

---
+++ SportNotepadMVC.Application/ViewModels/Goal/GoalForListVm.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using SportNotepadMVC.Application.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportNotepadMVC.Application.ViewModels.Goal
+{
+    public class GoalForListVm : IMapFrom<Domain.Model.Goal>
+    {
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+        public string Distance { get; set; }
+        public string Time { get; set; }
+        public bool Paid { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Domain.Model.Goal, GoalForListVm>();
+        }
+    }
+}
