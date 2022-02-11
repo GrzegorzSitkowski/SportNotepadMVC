cd7a3d7cb13c013fe05bcd49086a5b6bbe074535
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-02-11 18:36:36 +0100

    Added views of Login and Register

--- SportNotepadMVC.Web/obj/Debug/net5.0/SportNotepadMVC.Web.MvcApplicationPartsAssemblyInfo.cs
+++ SportNotepadMVC.Web/obj/Debug/net5.0/SportNotepadMVC.Web.MvcApplicationPartsAssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.ApiAuthorization.IdentityServer")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.Identity.UI")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.Identity.UI.Views.V4")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation")]
