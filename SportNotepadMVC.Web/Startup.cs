3288f0dc17ae8060795d83b98e85797b9d868273
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-02-11 22:55:14 +0100

    Configured registration for users

--- SportNotepadMVC.Web/Startup.cs
+++ SportNotepadMVC.Web/Startup.cs
@@ -41,6 +41,17 @@ namespace SportNotepadMVC.Web
             services.AddInfrastructure();
 
             services.AddControllersWithViews();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredUniqueChars = 1;
+
+                options.SignIn.RequireConfirmedEmail = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
