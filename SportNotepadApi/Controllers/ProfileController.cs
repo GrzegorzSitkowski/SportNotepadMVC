cdfd795db73d981b1c614316e38207a7d15333b9
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-04-25 13:39:33 +0200

    Added ProfileController in WebApi

---
+++ SportNotepadApi/Controllers/ProfileController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SportNotepadMVC.Application.Interfaces;
+using SportNotepadMVC.Application.ViewModels.ProfileUser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportNotepadApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        private readonly IProfileUserService _profileService;
+
+        public ProfileController(IProfileUserService profileService)
+        {
+            _profileService = profileService;
+        }
+
+        [HttpGet("Profile")]
+        public ActionResult GetProfile()
+        {
+            var result = _profileService.DisplayProfileUser();
+            if(result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        [HttpGet("Edit")]
+        public ActionResult EditProfile(NewProfileUserVm profileVm)
+        {
+            _profileService.EditProfileUser(profileVm);
+            return Ok();
+        }
+    }
+}
